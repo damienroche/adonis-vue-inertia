e7b94714ac565c8c87e6018b2a41d5c9a439a6e5
Damien Roche <damienroche@free.fr>
2024-10-21 14:31:45 +0200

    add unplugin components + auto-imports

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   css: {
@@ -16,6 +18,16 @@ export default defineConfig({
     inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }),
     vue(),
     adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] }),
+    Components({
+      dirs: ['inertia/components'],
+      dts: 'inertia/components.d.ts',
+    }),
+    AutoImport({
+      vueTemplate: true,
+      imports: ['vue'],
+      dirs: ['inertia/composables/**', 'inertia/lib/**'],
+      dts: 'inertia/auto-import.d.ts',
+    }),
   ],
 
   /**
