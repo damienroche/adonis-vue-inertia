666ac4a3c69ffd3cbc10a90242c99299d6d1a3b2
Damien Roche <damienroche@free.fr>
2024-10-21 21:54:03 +0200

    current page nav

--- inertia/layouts/Default.vue
+++ inertia/layouts/Default.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+
 import { Head, Link } from '@inertiajs/vue3'
 defineProps<{
   title: string
 }>()
 const { app } = usePageProps()
+const { url } = usePage()
 </script>
 
 <template>
@@ -11,7 +13,12 @@ const { app } = usePageProps()
   <div>
     <header class="max-w-screen-lg mx-auto py-4">
       <nav>
-        <Button as-child variant="ghost" v-for="link in app.nav" :key="link.href">
+        <Button
+          v-for="link in app.nav"
+          as-child
+          :key="link.href"
+          :variant="url === link.href ? 'default' : 'ghost'"
+        >
           <Link :href="link.href">{{ link.title }}</Link>
         </Button>
       </nav>
